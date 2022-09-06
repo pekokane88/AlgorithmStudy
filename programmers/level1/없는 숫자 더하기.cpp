322bc449f18a62576d7bf1781bec0b13930a4d96
pekokane88 <108170898+pekokane88@users.noreply.github.com>
2022-09-06 21:57:48 +0900

    Create 없는 숫자 더하기.cpp

---
+++ programmers/level1/없는 숫자 더하기.cpp
@@ -0,0 +1,27 @@
+/*
+  배열의 Index를 값으로 사용. bucket[i] 가 0이면 그 숮자는 없는 값이기에 더해주면 된다.
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> bucket(10, 0);
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    int numbersSize = numbers.size();
+    
+    for(int i = 0; i < numbersSize; i++){
+        bucket[numbers[i]]++;
+    }
+    
+    for(int i = 0; i<10; i++){
+        if(bucket[i] == 0){
+            answer += i;
+        }
+    }
+    
+    return answer;
+}
